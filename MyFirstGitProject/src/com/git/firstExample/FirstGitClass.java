5219f33311d00eb4c6402312db07ecbbb5183052
santosh <san.kaparthi@gmail.com>
2015-07-26 07:36:07 -0400

    comiting from windows

--- MyFirstGitProject/src/com/git/firstExample/FirstGitClass.java
+++ MyFirstGitProject/src/com/git/firstExample/FirstGitClass.java
@@ -5,6 +5,7 @@ public class FirstGitClass {
 	public static void main(String[] args) {
 		System.out.println("My First git hub project");
 		System.out.println("This is my second change");
+		System.out.println("This change is from windows");
 
 	}
 
